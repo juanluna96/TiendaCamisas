e1dbebbfc69e12658d65eaac24b82944f4d81c88
juanluna96 <juandluna17@gmail.com>
2020-10-10 16:17:27 -0500

    Anadida estructura HTML y Header

---
+++ includes/footer.php
@@ -0,0 +1,20 @@
+    <!-- End your project here-->
+    <!-- jQuery -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js">
+    </script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js">
+    </script>
+    <!-- PUGLINS -->
+    <script src="puglins/Sticky-Mobile-Navigation-GRT-Responsive-Menu/grt-responsive-menu.js"></script>
+    <!-- Your custom scripts (optional) -->
+    <script type="text/javascript">
+    </script>
+    </body>
+
+    </html>
---
+++ includes/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Material Design for Bootstrap</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Google Fonts Roboto -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+    <!-- PUGLINS -->
+    <link rel="stylesheet" href="puglins/Sticky-Mobile-Navigation-GRT-Responsive-Menu/grt-responsive-menu.css">
+    <!-- Your custom styles (optional) -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <!-- HEADER -->
+    <header>
+        <div class="menu-container">
+            <div class="grt-menu-row">
+                <div class="grt-menu-logo">
+                    <a href="#" class="grt-logo"><img src="img/logo_tienda_camisas_sin_fondo.png"></a>
+                </div>
+                <div class="grt-menu-right">
+                    <nav>
+                        <button class="grt-mobile-button"><span class="line1"></span><span class="line2"></span><span
+                                class="line3"></span></button>
+                        <ul class="grt-menu">
+                            <li class="active"><a href="">Inicio</a></li>
+                            <li class="grt-dropdown"><a>Categorias</a>
+                                <ul class="grt-dropdown-list">
+                                    <li><a href="">Menu 1</a></li>
+                                    <li><a href="">Menu 2</a></li>
+                                    <li><a href="">Menu 3</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="">Sobre nosotros</a></li>
+                            <li><a href="">Portafolio</a></li>
+                            <li><a href="">Blog</a></li>
+                            <li><a href="">Contacto</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- FIN HEADER -->
