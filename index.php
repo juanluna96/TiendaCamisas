324ab8768dbce9326fd1a8d7f5a651e34bf9884c
juanluna96 <juandluna17@gmail.com>
2020-10-15 22:32:59 -0500

    Login de usuarios

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
-<?php require_once "config/parameters.php";?>
-<?php session_start();?>
-<?php require_once "config/db.php";?>
-<?php require_once "autoload.php";?>
-<?php require_once "helpers/utilidades.php";?>
-<?php require_once 'views/layout/header.php';?>
+<?php require_once "config/parameters.php"; ?>
+<?php session_start(); ?>
+<?php require_once "config/db.php"; ?>
+<?php require_once "autoload.php"; ?>
+<?php require_once "helpers/utilidades.php"; ?>
+<?php require_once 'views/layout/header.php'; ?>
+<?php require_once "views/layout/modales.php"; ?>
 
 <?php // Conexion base de datos
-$db=Database::connect();?>
+$db = Database::connect(); ?>
 
 
 <div class="container-fluid mt-5 row pr-0 contenedor-principal" style="min-height: 100vh;">
 
-    <?php require_once 'views/layout/sidebar.php';?>
+    <?php require_once 'views/layout/sidebar.php'; ?>
 
     <!---------------------------------------------------------------------------- */
 /*                                    PRODUCTOS                                  */
@@ -19,39 +20,38 @@ $db=Database::connect();?>
     <div class="productos pr-0 col-12 col-lg-9 mt-3">
         <?php
 
-function mostrar_error()
-{
-    $error = new ErrorController();
-    $error->index();
-}
+        function mostrar_error()
+        {
+            $error = new ErrorController();
+            $error->index();
+        }
 
-if (isset($_GET['controller'])) {
-    $nombre_controlador = $_GET['controller'] . 'Controller';
-} elseif (!isset($_GET['controller']) && !isset($_GET['action'])) {
-    $nombre_controlador = controller_default;
-} else {
-    mostrar_error();
-    exit;
-}
+        if (isset($_GET['controller'])) {
+            $nombre_controlador = $_GET['controller'] . 'Controller';
+        } elseif (!isset($_GET['controller']) && !isset($_GET['action'])) {
+            $nombre_controlador = controller_default;
+        } else {
+            mostrar_error();
+            exit;
+        }
 
-if (isset($nombre_controlador) && class_exists($nombre_controlador)) {
+        if (isset($nombre_controlador) && class_exists($nombre_controlador)) {
 
-    $controlador = new $nombre_controlador();
+            $controlador = new $nombre_controlador();
 
-    if (isset($_GET['action']) && method_exists($controlador, $_GET['action'])) {
-        $action = $_GET['action'];
-        $controlador->$action();
-    } elseif (!isset($_GET['controller']) && !isset($_GET['action'])) {
-        $action_default = action_default;
-        $controlador->$action_default();
-    } else {
-        mostrar_error();
-    }
-
-} else {
-    mostrar_error();
-}
-?>
+            if (isset($_GET['action']) && method_exists($controlador, $_GET['action'])) {
+                $action = $_GET['action'];
+                $controlador->$action();
+            } elseif (!isset($_GET['controller']) && !isset($_GET['action'])) {
+                $action_default = action_default;
+                $controlador->$action_default();
+            } else {
+                mostrar_error();
+            }
+        } else {
+            mostrar_error();
+        }
+        ?>
 
     </div>
 </div>
@@ -60,4 +60,4 @@ if (isset($nombre_controlador) && class_exists($nombre_controlador)) {
 var enlace = "<?php echo base_url; ?>";
 </script>
 
-<?php require_once 'views/layout/footer.php';?>
+<?php require_once 'views/layout/footer.php'; ?>
