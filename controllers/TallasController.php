86fb46d6987f36496fbc7bc168abb0a8b843b8bd
juanluna96 <juandluna17@gmail.com>
2020-10-22 14:11:23 -0500

    Borrado de tallas

--- controllers/TallasController.php
+++ controllers/TallasController.php
@@ -30,16 +30,37 @@ class TallasController
                 $save = $talla->save();
 
                 if ($save) {
-                    $_SESSION["talla"] = "Registro_exitoso";
+                    $_SESSION["tallaje"] = "registrado_exitoso";
                 } else {
-                    $_SESSION["talla"] = "Registro_fallido";
+                    $_SESSION["tallaje"] = "registrado_fallido";
                 }
             } else {
-                $_SESSION["talla"] = "Registro_fallido";
+                $_SESSION["tallaje"] = "registrado_fallido";
             }
         } else {
-            $_SESSION["talla"] = "Registro_fallido";
+            $_SESSION["tallaje"] = "registrado_fallido";
         }
         header('location:' . base_url . "tallas/index");
+    }
+
+    public function eliminar()
+    {
+        utilidades::ValidarAdmin();
+        if (isset($_GET["talla_id"])) {
+            $id = $_GET['talla_id'];
+            $talla = new talla();
+            $talla->setId($id);
+            $delete = $talla->delete();
+
+            if ($delete) {
+                $_SESSION["tallaje"] = "borrado_exitoso";
+            } else {
+                $_SESSION["tallaje"] = "borrado_fallido";
+            }
+        } else {
+            $_SESSION["tallaje"] = "borrado_fallido";
+        }
+
+        header('location:' . base_url . 'tallas/index');
     }
 }
